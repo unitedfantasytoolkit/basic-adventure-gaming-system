790b952bce81e8f8498b523d6ca6a0e1a82b1948
wyrmisis <tim.snyder.work@gmail.com>
2025-01-27 23:55:40 -0500

    fix: Add debug console log for chat message rendering

--- src/chat/chat-message.document.mjs
+++ src/chat/chat-message.document.mjs
@@ -105,6 +105,8 @@ export default class BAGSChatMessage extends ChatMessage {
       messageData.borderColor = this.author?.color.css
     }
 
+    console.info(this)
+
     // Render the chat message
     let html = await renderTemplate(this.template, messageData)
     html = foundry.applications.parseHTML(html)
