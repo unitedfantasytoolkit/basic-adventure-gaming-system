0814332e4dfa8f634be634441ad79e7683039485
wyrmisis <tim.snyder.work@gmail.com>
2025-02-25 00:37:22 -0500

    refactor: Update actor fields with detailed comments and new biographical details

--- src/actors/actor.fields.mjs
+++ src/actors/actor.fields.mjs
@@ -24,6 +24,9 @@ export const saveOptions = {
 
 export const baseFactory = (fields) =>
   new SchemaField({
+    /**
+     * A creature's exploration speed.
+     */
     speed: new NumberField({
       min: 0,
       nullable: false,
@@ -65,8 +68,7 @@ export const modifiersFactory = (fields) => {
           ...obj,
           [s]: new NumberField({
             initial: 0,
-            label: "",
-            hint: "",
+            label: savingThrowSettings.savingThrows[s].label,
           }),
         }),
         {},
@@ -126,6 +128,9 @@ export const retainerFactory = (fields) =>
       hint: "",
     }),
     wage: new SchemaField({
+      /**
+       * The Item to use as currency for this Actor's wage.
+       */
       currency: new DocumentUUIDField({
         type: "Item",
         initial: DEFAULT_GOLD_ITEM_UUID,
@@ -133,10 +138,21 @@ export const retainerFactory = (fields) =>
         label: "",
         hint: "",
       }),
+      /**
+       * How much of this wage is paid.
+       */
       quantity: new NumberField({
         min: 0,
         initial: 0,
         blank: false,
+        label: "",
+        hint: "",
+      }),
+      /**
+       * How often the Actor is paid.
+       */
+      interval: new StringField({
+        initial: "",
         label: "",
         hint: "",
       }),
@@ -146,12 +162,18 @@ export const retainerFactory = (fields) =>
 
 export const hpFactory = (fields) =>
   new SchemaField({
+    /**
+     * How many HP the Actor has right now.
+     */
     value: new NumberField({
       initial: 4,
       blank: false,
       label: "",
       hint: "",
     }),
+    /**
+     * How many HP the Actor has when fully healed.
+     */
     max: new NumberField({
       initial: 4,
       blank: false,
@@ -159,6 +181,9 @@ export const hpFactory = (fields) =>
       label: "",
       hint: "",
     }),
+    /**
+     * How many temporary HP the Actor has -- a buffer before losing HP.
+     */
     temporary: new NumberField({
       initial: 0,
       blank: false,
@@ -171,19 +196,49 @@ export const hpFactory = (fields) =>
 
 export const biographicalDetailsFactory = (fields) =>
   new SchemaField({
+    /**
+     * The Actor's alignment. We don't use choices here so users can freely
+     * enter whatever they like (Chaotic, Lawful Good, a phase of the moon)
+     */
     alignment: new StringField({
       nullable: false,
       initial: "",
       label: "",
       hint: "",
     }),
-    identity: new StringField({
+    /**
+     * A prefix attached to the Actor's name. Examples: "Sir/Dame", "Arbiter",
+     * "Agent"
+     */
+    identityPrefix: new StringField({
       nullable: false,
       initial: "",
       label: "",
       hint: "",
     }),
+    /**
+     * A suffix attached to the Actor's name. Examples: "of Clan Bearhair",
+     * ", butcher of Bogtown", "the Smelly"
+     */
+    identitySuffix: new StringField({
+      nullable: false,
+      initial: "",
+      label: "",
+      hint: "",
+    }),
+    /**
+     * A physical description of the Actor.
+     */
     description: new HTMLField({
+      nullable: false,
+      initial: "",
+      label: "",
+      hint: "",
+    }),
+    /**
+     * The character's background.
+     */
+    background: new HTMLField({
       nullable: false,
       initial: "",
       label: "",
@@ -193,6 +248,9 @@ export const biographicalDetailsFactory = (fields) =>
   })
 
 export const bannerFactory = () =>
+  /**
+   * A banner image, used to add some flair to Actors.
+   */
   new FilePathField({
     required: false,
     categories: ["IMAGE"],
